b54c097393b0c793c448d466de450503d47247f6
Ethan Fuerst <ethanfuerst@gmail.com>
2022-09-22 22:47:24 -0400

    moving dict to different line

--- freeze_and_deploy.py
+++ freeze_and_deploy.py
@@ -26,14 +26,6 @@ if __name__ == "__main__":
 
     bucket = s3.Bucket(AWS_BUCKET)
 
-    rep_dict = {
-        ".css": "text/css",
-        ".js": "text/javascript",
-        ".html": "text/html",
-        ".svg": "image/svg+xml",
-        ".woff": "application/font-woff",
-    }
-
     def file_type_finder(file):  # can't find a more pythonic way to do this
         if file.endswith(".css"):
             return "text/css"
@@ -48,10 +40,12 @@ if __name__ == "__main__":
         else:
             return ""
 
-    for file_name, file_type in {f: file_type_finder(f) for f in all_files}.items():
-        print(f'uploading {file_name} to {file_name.replace(BUILD_DIRECTORY, "")}')
+    file_names_and_types = {f: file_type_finder(f) for f in all_files}
+
+    for file_name, file_type in file_names_and_types.items():
         bucket.upload_file(
             file_name,
             file_name.replace(BUILD_DIRECTORY, ""),
             ExtraArgs={"ContentType": file_type},
         )
+        print(f'Uploaded {file_name} to {file_name.replace(BUILD_DIRECTORY, "")}')
